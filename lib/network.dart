faec238fa43f55af8f6fe326855aa63ba517f397
DenUp <ctrike163@gmail.com>
2026-02-16 21:21:34 +0300

    указал явно путь

--- lib/network.dart
+++ lib/network.dart
@@ -4,8 +4,8 @@ import 'package:network/user_service.dart';
 export 'user_service.dart';
 
 class Network {
-  late final dio;
-  late final userService;
+  late final Dio dio;
+  late final UserService userService;
 
   Network({String baseUrl = 'https://api.matule.ru/api'}) {
     dio = Dio(BaseOptions(baseUrl: baseUrl));
--- lib/user_service.dart
+++ lib/user_service.dart
@@ -1,21 +1,21 @@
-import 'package:dio/dio.dart';
-import 'package:network/entity/auth.dart';
+  import 'package:dio/dio.dart';
+  import 'package:network/entity/auth.dart';
 
-class UserService {
-  final Dio _dio;
+  class UserService {
+    final Dio _dio;
 
-  UserService({required Dio dio}) : _dio = dio;
+    UserService({required Dio dio}) : _dio = dio;
 
-  Future<Auth> authWithPassword({
-    required String email,
-    required String password,
-  }) async {
-    final data = {"identity": email, "password": password};
-    final response = await _dio.post(
-      '/collections/users/auth-with-password',
-      data: data,
-    );
+    Future<Auth> authWithPassword({
+      required String email,
+      required String password,
+    }) async {
+      final data = {"identity": email, "password": password};
+      final response = await _dio.post(
+        '/collections/users/auth-with-password',
+        data: data,
+      );
 
-    return Auth.fromJson(response.data);
+      return Auth.fromJson(response.data);
+    }
   }
-}
